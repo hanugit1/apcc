66850db68a83fa7ffc5c905aefd78b0493637192
hanuman <hanuman.tech1@gmail.com>
2018-07-14 00:10:02 +0530

    Creating RegisterSvcProcessImpl,RegisterSvcReqBuilder,RegisterSvcResBuilder and RegisterSvcResBuilder

--- Services/SOAP/RegisterSvc_ProcessLayer/src/main/java/org/o2/registersvc/process/impl/RegisterServiceProcessImpl.java
+++ Services/SOAP/RegisterSvc_ProcessLayer/src/main/java/org/o2/registersvc/process/impl/RegisterServiceProcessImpl.java
@@ -1,6 +1,13 @@
 package org.o2.registersvc.process.impl;
 
+import org.o2.registersvc.creditcheck.CreditCheckSvcClient;
+import org.o2.registersvc.creditcheck.beans.CreditCheckReq;
+import org.o2.registersvc.creditcheck.beans.CreditCheckRes;
+import org.o2.registersvc.creditcheck.impl.CreditCheckSvcClientImpl;
+import org.o2.registersvc.intg.RegisterServiceDAO;
 import org.o2.registersvc.intg.beans.RegisterServiceDAOReq;
+import org.o2.registersvc.intg.beans.RegisterServiceDAORes;
+import org.o2.registersvc.intg.impl.RegisterServiceDAOImpl;
 import org.o2.registersvc.process.RegisterServiceProcess;
 import org.o2.registersvc.process.beans.RegisterSvcProcessVBReq;
 import org.o2.registersvc.process.beans.RegisterSvcProcessVBRes;
@@ -19,10 +26,30 @@ public class RegisterServiceProcessImpl implements RegisterServiceProcess {
 		daoReq.setNameOnCard(vbReq.getNameOnCard());
 		daoReq.setAccountNum(vbReq.getAccountNum());
 		daoReq.setMobileNum(vbReq.getMobileNum());
+		
 		//3.Call the CreditCheck based on CreditCheck Response
+		CreditCheckSvcClient ccSvcClient = new CreditCheckSvcClientImpl();
+		CreditCheckReq ccReq = new CreditCheckReq();
+		ccReq.setCardNum(vbReq.getCardNum());
+		ccReq.setCvv(vbReq.getCvv());
+		ccReq.setExpDate(vbReq.getExpDate());
+		ccReq.setNameOnCard(vbReq.getNameOnCard());
+		ccReq.setClientId(vbReq.getClientId());
+		ccReq.setChannelId(vbReq.getChannelId());
 		
-		
-		return null;
+		CreditCheckRes ccResp = ccSvcClient.getCreditScore(ccReq);
+		if(ccResp != null && ccResp.getScore() > 60) {
+			//4.Call the Dao
+			RegisterServiceDAO dao = new RegisterServiceDAOImpl();
+			RegisterServiceDAORes daoResp = dao.enrollment(daoReq);
+		}
+		//5.Prepare the VbResp i.e., convert daoResp to Process Response
+		RegisterSvcProcessVBRes vbResp = new RegisterSvcProcessVBRes();
+		vbResp.setRespCode(ccResp.getRespCode());
+		vbResp.setRespMsg(ccResp.getRespMsg());
+		vbResp.setScore(ccResp.getScore());
+		vbResp.setStatus(ccResp.getStatus());
+		return vbResp;
 	}
 
 }
