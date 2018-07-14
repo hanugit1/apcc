d3cf999daa5bedde89cd9577cab4506e5020dade
hanuman <hanuman.tech1@gmail.com>
2018-07-14 23:11:56 +0530

    Added Documents

---
+++ Services/SOAP/RegisterSvc_ServiceLayer/src/main/java/org/o2/registersvc/service/builder/RegisterServiceResBuilder.java
@@ -0,0 +1,23 @@
+package org.o2.registersvc.service.builder;
+
+import org.o2.registersvc.process.beans.RegisterSvcProcessVBRes;
+import org.o2.registersvc.schema.res.CreditCheckType;
+import org.o2.registersvc.schema.res.RegisterServiceResType;
+import org.o2.registersvc.schema.res.StatusBlockType;
+
+public class RegisterServiceResBuilder {
+	public RegisterServiceResType buildWsResp(RegisterSvcProcessVBRes vbResp) {
+		RegisterServiceResType wsResp = new RegisterServiceResType();
+		// Prepare the StatusBlock
+		StatusBlockType stBlock = new StatusBlockType();
+		stBlock.setRespCode(vbResp.getRespCode());
+		stBlock.setRespMsg(vbResp.getRespMsg());
+
+		// Prepare the CreditCheckType Obj//
+		CreditCheckType creditCheck = new CreditCheckType();
+		creditCheck.setCreditStatus(vbResp.getStatus());
+		creditCheck.setRateOfPer(vbResp.getScore());
+		return wsResp;
+
+	}
+}
