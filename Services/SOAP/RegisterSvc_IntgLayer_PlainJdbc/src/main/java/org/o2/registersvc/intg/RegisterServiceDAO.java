c42150653c5b1a8fbdae7b4082cc9f9c1217445a
hanuman <hanuman.tech1@gmail.com>
2018-07-25 10:19:01 +0530

    Added Dependencies and packages and classes to RegisterSvc_IntgLayer_PlainJdbc

--- Services/SOAP/RegisterSvc_IntgLayer_PlainJdbc/src/main/java/org/o2/registersvc/intg/RegisterServiceDAO.java
+++ Services/SOAP/RegisterSvc_IntgLayer_PlainJdbc/src/main/java/org/o2/registersvc/intg/RegisterServiceDAO.java
@@ -2,12 +2,12 @@ package org.o2.registersvc.intg;
 
 import java.io.FileNotFoundException;
 
-import org.o2.registersvc.intg.beans.RegisterServiceDAOReq;
-import org.o2.registersvc.intg.beans.RegisterServiceDAORes;
+import org.o2.registersvc.intg.beans.RegisterSvcDAOReq;
+import org.o2.registersvc.intg.beans.RegisterSvcDAORes;
 import org.o2.registersvc.intg.util.BusinessException;
 import org.o2.registersvc.intg.util.SystemException;
 import org.o2.registersvc.intg.util.UnknownException;
 
 public interface RegisterServiceDAO {//
-	public RegisterServiceDAORes enrollment(RegisterServiceDAOReq daoReq) throws FileNotFoundException, BusinessException, SystemException, UnknownException;
+	public RegisterSvcDAORes enrollment(RegisterSvcDAOReq daoReq) throws FileNotFoundException, BusinessException, SystemException, UnknownException;
 }
