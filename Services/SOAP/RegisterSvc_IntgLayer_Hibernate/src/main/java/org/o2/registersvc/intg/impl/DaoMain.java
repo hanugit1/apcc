56bcd05329ae691df4f9b1a192223ba0525d7280
hanuman <hanuman.tech1@gmail.com>
2018-07-26 11:52:11 +0530

    Modifications

--- Services/SOAP/RegisterSvc_IntgLayer_Hibernate/src/main/java/org/o2/registersvc/intg/impl/DaoMain.java
+++ Services/SOAP/RegisterSvc_IntgLayer_Hibernate/src/main/java/org/o2/registersvc/intg/impl/DaoMain.java
@@ -13,72 +13,36 @@ import java.util.Properties;
 
 import javax.net.ssl.SSLEngineResult.Status;
 
-public class DaoMain {
-	public static void main(String[] args) throws SQLException, IOException {
-		Connection con;
-		CallableStatement cs = null;
-		
-		try {
-			/*FileReader reader = new FileReader("src/main/resources/db.properties");
-			Properties prop = new Properties();
-			prop.load(reader);
-			String driverClass = prop.getProperty("driverClass");
-			String url = prop.getProperty("url");
-			String uname = prop.getProperty("uname");
-			String pwd = prop.getProperty("pwd");
-			
-			Class.forName(driverClass);
-			con = DriverManager.getConnection(url, uname, pwd);*/
-			
-			Class.forName("com.mysql.jdbc.Driver");
-			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/hanu", "root", "root");
-			
-			String sql = "{call GET_ENROLL(?,?,?,?,?,?,?,?,?,?)}";
-			cs = con.prepareCall(sql);
-			//Register Output parameters with JDBC types
-			cs.registerOutParameter(9, Types.VARCHAR);
-			cs.registerOutParameter(10, Types.VARCHAR);
-			
-			//Set values to input parameters
-			cs.setString(1, "online");
-			cs.setString(2, "web");
-			cs.setString(3, "885744998856");
-			cs.setString(4, "343");
-			cs.setString(5, "19/02/2025");
-			cs.setString(6, "mahesh");
-			cs.setString(7, "679844997418");
-			cs.setInt(8, 998866776);
-			boolean b = cs.execute();
-			String respCode = cs.getString(9);
-			String respMsg = cs.getString(10);
-			
-			System.out.println("Status code is "+cs.getString(9));
-			System.out.println("Status msg is "+cs.getString(10));
-			
-			//Creating StatusBlockDao obj
-			/*StatusBlockDao statusBlock = new StatusBlockDao();
-			statusBlock.setRespCode(respCode);
-			statusBlock.setRespMsg(respMsg);*/
+import org.o2.registersvc.intg.beans.RegisterSvcDAOReq;
+import org.o2.registersvc.intg.beans.RegisterSvcDAORes;
+import org.o2.registersvc.intg.util.BusinessException;
+import org.o2.registersvc.intg.util.SystemException;
+import org.o2.registersvc.intg.util.UnknownException;
 
-			
-		}catch(SQLException e) {
+public class DaoMain {
+	public static void main(String[] args) throws FileNotFoundException, SQLException  {
+		RegisterSvcDAOReq daoReq = new RegisterSvcDAOReq();
+		daoReq.setAccountNum("12323");
+		daoReq.setCardNum("445445");
+		daoReq.setChannelId("web");
+		daoReq.setClientId("online");
+		daoReq.setCvv("434");
+		daoReq.setExpDate("09/1/25");
+		daoReq.setMobileNum(889988444);
+		daoReq.setNameOnCard("raghu");
+
+		RegisterSvcDAOImpl daoImpl = new RegisterSvcDAOImpl();
+		try {
+			RegisterSvcDAORes daoResp = daoImpl.enrollment(daoReq);
+		} catch (BusinessException e) {
 			e.printStackTrace();
-		}catch (ClassNotFoundException e) {
+		} catch (SystemException e) {
 			e.printStackTrace();
-		}
-		//
-		ResultSet rs = cs.executeQuery("select *from customer_details");
-		System.out.println("ResultSet object "+rs);
-		
-		while (rs.next()) {
-			System.out.println(rs.getString("sno"));
-			System.out.println(rs.getString("card_num"));
-			System.out.println(rs.getString("cvv"));
-			System.out.println(rs.getString("exp_date"));
-			System.out.println(rs.getString("name_on_card"));
-			System.out.println(rs.getInt("mob_num"));
-			
-		}
-		
+		} catch (UnknownException e) {
+			e.printStackTrace();
+		} /*catch (SQLException e) {
+			e.printStackTrace();
+		}*/
+
 	}
 }
--- Services/SOAP/RegisterSvc_IntgLayer_Hibernate/src/main/java/org/o2/registersvc/intg/impl/RegisterSvcDAOImpl.java
+++ Services/SOAP/RegisterSvc_IntgLayer_Hibernate/src/main/java/org/o2/registersvc/intg/impl/RegisterSvcDAOImpl.java
@@ -2,51 +2,104 @@ package org.o2.registersvc.intg.impl;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.util.List;
 import java.util.Properties;
 
+import javax.security.auth.login.CredentialExpiredException;
+
+import org.apache.log4j.Logger;
+import org.hibernate.Criteria;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.criterion.Restrictions;
+import org.o2.registersvc.entities.AccountDetails;
+import org.o2.registersvc.entities.ChannelDetails;
+import org.o2.registersvc.entities.ClientDetails;
+import org.o2.registersvc.entities.CustomerDetails;
 import org.o2.registersvc.intg.RegisterSvcDAO;
 import org.o2.registersvc.intg.beans.RegisterSvcDAOReq;
 import org.o2.registersvc.intg.beans.RegisterSvcDAORes;
 import org.o2.registersvc.intg.util.BusinessException;
+import org.o2.registersvc.intg.util.HibernateUtility;
 import org.o2.registersvc.intg.util.SystemException;
 import org.o2.registersvc.intg.util.UnknownException;
 
 public class RegisterSvcDAOImpl implements RegisterSvcDAO {
 
-	public RegisterSvcDAORes enrollment(RegisterSvcDAOReq daoReq) throws FileNotFoundException, BusinessException, SystemException, UnknownException {
-		// 1.Get the Req from Process layer
-		// 2.Prepare the Req for Db
-		// 3.Call Db and get ResultSet
-		// 4.Prepare the DaoResp from ResultSet obj
+	private static Logger logger = Logger.getLogger(RegisterSvcDAOImpl.class);
+	RegisterSvcDAORes daoResp = null;
 
-		String env = System.getProperty("env");
-		Properties prop = new Properties();
-		FileReader reader = new FileReader("properties/enrolldb_" + env + ".properties");
-		// prop.load(reader);
+	public RegisterSvcDAORes enrollment(RegisterSvcDAOReq daoReq)
+			throws FileNotFoundException, BusinessException, SystemException, UnknownException {
 
-		RegisterSvcDAORes daoResp = null;
-		try {//
-			String dbRespCode = "1111";
-			String dbRespMsg = "Success";
-			if ("0".equals(dbRespCode)) {
-				daoResp = new RegisterSvcDAORes();
-				daoResp.setRespCode(dbRespCode);
-				daoResp.setRespMsg(dbRespMsg);
-			} else if ("1001".equals(dbRespCode) || "1002".equals(dbRespCode)) {
-				throw new BusinessException(dbRespCode, dbRespMsg);
-			} else if ("1111".equals(dbRespCode) || "2222".equals(dbRespCode)) {
-				throw new SystemException(dbRespCode, dbRespMsg);
-			} else {
-				throw new UnknownException(dbRespCode, dbRespMsg);
+		SessionFactory sFactory = null;
+		Session session = null;
+		Transaction txn = null;
+		try {
+			// Create SessionFactory
+			sFactory = HibernateUtility.getSessionFactory();
+
+			// Create Session Object
+			session = sFactory.openSession();
+
+			// Create Transaction
+			txn = session.beginTransaction();
+
+			// Check client_id
+			Criteria criteria2 = session.createCriteria(ClientDetails.class);
+			criteria2.add(Restrictions.eq("clientId", daoReq.getClientId()));
+			List clientId = criteria2.list();
+
+			// Check channel_id
+			Criteria criteria3 = session.createCriteria(ChannelDetails.class);
+			criteria3.add(Restrictions.eq("channelId", daoReq.getChannelId()));
+			List channelId = criteria3.list();
+
+			Query query = session.createQuery("from AccountDetails where accId=? and mobNum=?");
+			query.setString(0, daoReq.getAccountNum());
+			query.setLong(1, daoReq.getMobileNum());
+			List<AccountDetails> accDetails = query.list();
+
+			if (clientId.size() > 0 && channelId.size() > 0 && accDetails.size() > 0) {
+				// Create CustomerDetails Object
+				CustomerDetails custDetails = new CustomerDetails();
+
+				// Set CutometDetails values
+				custDetails.setCardNum(daoReq.getCardNum());
+				custDetails.setCvv(daoReq.getCvv());
+				custDetails.setExpDate(daoReq.getExpDate());
+				custDetails.setMobNum(daoReq.getMobileNum());
+				custDetails.setNameOnCard(daoReq.getNameOnCard());
+
+				Object sno = session.save(custDetails);
+				txn.commit();
+				if (sno != null) {
+					daoResp = new RegisterSvcDAORes();
+					daoResp.setRespCode("0000");
+					daoResp.setRespMsg("Success");
+				} else if (clientId.size() == 0) {
+					daoResp = new RegisterSvcDAORes();
+					daoResp.setRespCode("1001");
+					daoResp.setRespMsg("Invalid clientId");
+				} else if (channelId.size() == 0) {
+					daoResp = new RegisterSvcDAORes();
+					daoResp.setRespCode("1002");
+					daoResp.setRespMsg("Invalid channelId");
+				} else if (accDetails.size() == 0) {
+					daoResp = new RegisterSvcDAORes();
+					daoResp.setRespCode("1003");
+					daoResp.setRespMsg("Invalid accountDetails");
+				}
+				System.out.println(daoResp);
+
 			}
-		} catch (BusinessException e) {
-			throw e;
-		} catch (SystemException e) {
-			throw e;
-		} catch (UnknownException e) {
-			throw e;
+		} catch (Exception e) {
+			txn.rollback();
 		}
-		return daoResp;
-	}
 
+		return daoResp;
+
+	}
 }
